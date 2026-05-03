50386d675089c70f7ae880a26f3aa372ed529ac6
Rutwik Balkhande <rutwikbalkhande42@gmail.com>
2026-05-04 01:47:34 +0530

    add pattern program

--- src/DSA_Array/sort/a.java
+++ src/DSA_Array/sort/a.java
@@ -6,31 +6,5 @@ public class a {
 
     static void bubble(int []arr){
 
-       // int[] arr={49,74,36,18,13,20}
-
-    }
-
-    public static void main(String[] args) {
-        int[] arr={49,74,36,18,13,20};
-<<<<<<< HEAD
-        bubble(arr);
-
-=======
-       // bubble(arr);
-
-        for(int i=0;i< arr.length; i++) {
-
-            int min=i;
-            for(int j=i+1; j<arr.length;j++){
-                if(arr[j]< arr[min]){
-                        min = j;
-                }
-            }
-              int tem= arr[min];
-            arr[min] = arr[i];
-            arr[i]= tem;
-        }
->>>>>>> db92610 (add new prgm)
-        System.out.println(Arrays.toString(arr));
     }
 }
